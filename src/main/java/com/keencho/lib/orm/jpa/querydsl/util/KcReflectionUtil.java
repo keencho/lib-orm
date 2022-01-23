aa13192136b3ea1e3bd97e3a664a96acc4be63c4
keencho <seyoung050412@gmail.com>
2022-01-23 23:17:15 +0900

    update

--- src/main/java/com/keencho/lib/orm/jpa/querydsl/util/KcReflectionUtil.java
+++ src/main/java/com/keencho/lib/orm/jpa/querydsl/util/KcReflectionUtil.java
@@ -2,7 +2,6 @@ package com.keencho.lib.orm.jpa.querydsl.util;
 
 import com.querydsl.core.types.ConstructorExpression;
 import com.querydsl.core.types.Path;
-import com.querydsl.core.types.dsl.BeanPath;
 import com.querydsl.core.types.dsl.EntityPathBase;
 
 import java.lang.reflect.Constructor;
@@ -10,7 +9,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class KcReflectionUtil {
