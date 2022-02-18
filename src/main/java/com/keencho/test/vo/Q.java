3459a09a809d3d8dac857eda831512ee5c8cd55f
keencho <seyoung050412@gmail.com>
2022-02-18 23:03:30 +0900

    del q object

--- src/main/java/com/keencho/test/vo/Q.java
+++
@@ -1,9 +0,0 @@
-package com.keencho.test.vo;
-
-import com.keencho.test.model.QMainOrder;
-import com.keencho.test.model.QRider;
-
-public class Q {
-    public static QMainOrder mainOrder = QMainOrder.mainOrder;
-    public static QRider rider = QRider.rider;
-}
